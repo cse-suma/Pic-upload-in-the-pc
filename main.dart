5a5dafb9ad879448af630e7ba4a523a0edb2c5c9
Suma Akter <102320034+cse-suma@users.noreply.github.com>
2024-08-10 16:53:18 +0600

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,23 @@
+//AssetImage upload done
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Image(
+            image: AssetImage(' pic/suma.jpg'),
+          ),
+        ),
+      ),
+    );
+  }
+}
